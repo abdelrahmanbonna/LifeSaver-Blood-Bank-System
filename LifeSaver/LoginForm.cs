ccf3d2e4067062181b55b2f4374e90cc21231f69
Abdelrahman Bonna <abdelrahmanbonna@outlook.com>
2020-11-24 00:41:12 +0200

    Add sign in and desgin login and home.

--- LifeSaver/LoginForm.cs
+++ LifeSaver/LoginForm.cs
@@ -16,5 +16,37 @@ namespace LifeSaver
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (UserOperations.Login(textBox1.Text,textBox2.Text)){
+                UserHomeForm uh = new UserHomeForm();
+                uh.Show();
+                this.Hide();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           if(BloodbankOperations.Login(textBox1.Text, textBox2.Text)){
+                BloodBankHomeForm bb = new BloodBankHomeForm();
+                bb.Show();
+                this.Hide();
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ForgetpasswordForm ff = new ForgetpasswordForm();
+            ff.Show();
+            this.Hide();
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            SignupForm signup = new SignupForm();
+            signup.Show();
+            this.Hide();
+        }
     }
 }

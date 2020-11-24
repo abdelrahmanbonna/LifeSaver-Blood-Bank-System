b6d454ce71d210b916ab0bd982b4c9d7c3d75bc1
Fatma <50721861+Fatma3598@users.noreply.github.com>
2020-11-24 20:23:02 +0200

    Update RequestBloodForm.Designer.cs

--- LifeSaver/RequestBloodForm.Designer.cs
+++ LifeSaver/RequestBloodForm.Designer.cs
@@ -28,12 +28,229 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.typeLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.passLabel = new System.Windows.Forms.Label();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.amountLabel = new System.Windows.Forms.Label();
+            this.BBName = new System.Windows.Forms.Label();
+            this.AmountBox = new System.Windows.Forms.TextBox();
+            this.TypesCombo = new System.Windows.Forms.ComboBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.bbnameCombo = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Maroon;
+            this.panel2.Location = new System.Drawing.Point(-1, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(481, 27);
+            this.panel2.TabIndex = 106;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Location = new System.Drawing.Point(-1, 424);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(481, 27);
+            this.panel1.TabIndex = 107;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(35, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 31);
+            this.button1.TabIndex = 108;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // typeLabel
+            // 
+            this.typeLabel.AutoSize = true;
+            this.typeLabel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.typeLabel.ForeColor = System.Drawing.Color.Maroon;
+            this.typeLabel.Location = new System.Drawing.Point(34, 257);
+            this.typeLabel.Name = "typeLabel";
+            this.typeLabel.Size = new System.Drawing.Size(113, 19);
+            this.typeLabel.TabIndex = 109;
+            this.typeLabel.Text = "Blood Types ";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailLabel.ForeColor = System.Drawing.Color.Maroon;
+            this.emailLabel.Location = new System.Drawing.Point(34, 115);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(66, 19);
+            this.emailLabel.TabIndex = 110;
+            this.emailLabel.Text = "E-mail ";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(197, 73);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(239, 20);
+            this.NameBox.TabIndex = 111;
+            // 
+            // passLabel
+            // 
+            this.passLabel.AutoSize = true;
+            this.passLabel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passLabel.ForeColor = System.Drawing.Color.Maroon;
+            this.passLabel.Location = new System.Drawing.Point(34, 166);
+            this.passLabel.Name = "passLabel";
+            this.passLabel.Size = new System.Drawing.Size(92, 19);
+            this.passLabel.TabIndex = 112;
+            this.passLabel.Text = "Password ";
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(197, 116);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(239, 20);
+            this.emailBox.TabIndex = 113;
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(197, 164);
+            this.passBox.Name = "passBox";
+            this.passBox.Size = new System.Drawing.Size(239, 20);
+            this.passBox.TabIndex = 114;
+            // 
+            // amountLabel
+            // 
+            this.amountLabel.AutoSize = true;
+            this.amountLabel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amountLabel.ForeColor = System.Drawing.Color.Maroon;
+            this.amountLabel.Location = new System.Drawing.Point(34, 298);
+            this.amountLabel.Name = "amountLabel";
+            this.amountLabel.Size = new System.Drawing.Size(73, 19);
+            this.amountLabel.TabIndex = 115;
+            this.amountLabel.Text = "Amount";
+            // 
+            // BBName
+            // 
+            this.BBName.AutoSize = true;
+            this.BBName.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BBName.ForeColor = System.Drawing.Color.Maroon;
+            this.BBName.Location = new System.Drawing.Point(34, 209);
+            this.BBName.Name = "BBName";
+            this.BBName.Size = new System.Drawing.Size(147, 19);
+            this.BBName.TabIndex = 116;
+            this.BBName.Text = "Bloodbank Name";
+            // 
+            // AmountBox
+            // 
+            this.AmountBox.Location = new System.Drawing.Point(197, 299);
+            this.AmountBox.Name = "AmountBox";
+            this.AmountBox.Size = new System.Drawing.Size(239, 20);
+            this.AmountBox.TabIndex = 117;
+            // 
+            // TypesCombo
+            // 
+            this.TypesCombo.FormattingEnabled = true;
+            this.TypesCombo.Items.AddRange(new object[] {
+            "A_pos",
+            "A_neg",
+            "B_pos",
+            "B_neg",
+            "AB_pos",
+            "AB_neg",
+            "O_pos",
+            "O_neg"});
+            this.TypesCombo.Location = new System.Drawing.Point(197, 258);
+            this.TypesCombo.Name = "TypesCombo";
+            this.TypesCombo.Size = new System.Drawing.Size(239, 21);
+            this.TypesCombo.TabIndex = 119;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.ForeColor = System.Drawing.Color.Maroon;
+            this.nameLabel.Location = new System.Drawing.Point(34, 72);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(56, 19);
+            this.nameLabel.TabIndex = 120;
+            this.nameLabel.Text = "Name";
+            // 
+            // bbnameCombo
+            // 
+            this.bbnameCombo.FormattingEnabled = true;
+            this.bbnameCombo.Location = new System.Drawing.Point(197, 209);
+            this.bbnameCombo.Name = "bbnameCombo";
+            this.bbnameCombo.Size = new System.Drawing.Size(239, 21);
+            this.bbnameCombo.TabIndex = 121;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Maroon;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Location = new System.Drawing.Point(367, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 31);
+            this.button2.TabIndex = 118;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // RequestBloodForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(481, 453);
+            this.Controls.Add(this.bbnameCombo);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.TypesCombo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.AmountBox);
+            this.Controls.Add(this.BBName);
+            this.Controls.Add(this.amountLabel);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.passLabel);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.typeLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "RequestBloodForm";
             this.Text = "RequestBloodForm";
+            this.Load += new System.EventHandler(this.RequestBloodForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label typeLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Label passLabel;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Label amountLabel;
+        private System.Windows.Forms.Label BBName;
+        private System.Windows.Forms.TextBox AmountBox;
+        private System.Windows.Forms.ComboBox TypesCombo;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.ComboBox bbnameCombo;
+        private System.Windows.Forms.Button button2;
     }
 }

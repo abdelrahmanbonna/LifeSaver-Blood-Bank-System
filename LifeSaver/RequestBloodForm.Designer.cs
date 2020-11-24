81fcafc3296b6940b1321c65b7ab25f6a442bd0c
Fatma <50721861+Fatma3598@users.noreply.github.com>
2020-11-24 21:54:21 +0200

    Update RequestBloodForm.Designer.cs

--- LifeSaver/RequestBloodForm.Designer.cs
+++ LifeSaver/RequestBloodForm.Designer.cs
@@ -204,6 +204,7 @@
             this.button2.TabIndex = 118;
             this.button2.Text = "Send";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // RequestBloodForm
             // 

bf8b84cb24ffd12851fcec390249aa79de1be5b5
Fatma <50721861+Fatma3598@users.noreply.github.com>
2020-11-24 20:23:05 +0200

    Update RequestBloodForm.cs

--- LifeSaver/RequestBloodForm.cs
+++ LifeSaver/RequestBloodForm.cs
@@ -16,5 +16,26 @@ namespace LifeSaver
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UserOperations.Request_Blood(NameBox.Text, emailBox.Text, bbnameCombo.Text, TypesCombo.Text, int.Parse(AmountBox.Text));
+        }
+
+        private void RequestBloodForm_Load(object sender, EventArgs e)
+        {
+            string Quarey = "select bb_name from BloodBank";
+            bbnameCombo.DataSource = DatabaseHandler.getMultiRows(Quarey);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
